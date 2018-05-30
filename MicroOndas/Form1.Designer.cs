a3a55109755f841dcb9e5d3fcba6ff65bc020b3a
Jordan Silva <jordanrafael92@gmail.com>
2018-05-30 15:49:50 -0300

    Projeto MicroOndas - LayOut com os botões

---
+++ MicroOndas/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace MicroOndas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textTimer = new System.Windows.Forms.TextBox();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPotencia = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textAlimento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 217);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+30 Ligar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textTimer
+            // 
+            this.textTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTimer.Location = new System.Drawing.Point(242, 150);
+            this.textTimer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textTimer.Name = "textTimer";
+            this.textTimer.Size = new System.Drawing.Size(132, 22);
+            this.textTimer.TabIndex = 1;
+            this.textTimer.Text = "0";
+            this.textTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimer.Location = new System.Drawing.Point(271, 69);
+            this.labelTimer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(0, 29);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 152);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Informe o Tempo de Cozimento";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Potência";
+            // 
+            // textPotencia
+            // 
+            this.textPotencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPotencia.Location = new System.Drawing.Point(242, 185);
+            this.textPotencia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textPotencia.Name = "textPotencia";
+            this.textPotencia.Size = new System.Drawing.Size(132, 22);
+            this.textPotencia.TabIndex = 4;
+            this.textPotencia.Text = "10";
+            this.textPotencia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 217);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ligar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(103, 261);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(242, 261);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 36);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Pausar/Continuar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 76);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tempo de Cozimento";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(103, 349);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 34);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Frango";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 123);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Informe o Alimento Desejado";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textAlimento
+            // 
+            this.textAlimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAlimento.Location = new System.Drawing.Point(242, 120);
+            this.textAlimento.Margin = new System.Windows.Forms.Padding(4);
+            this.textAlimento.Name = "textAlimento";
+            this.textAlimento.Size = new System.Drawing.Size(130, 22);
+            this.textAlimento.TabIndex = 12;
+            this.textAlimento.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(389, 123);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 13;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(240, 349);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(147, 34);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Arroz";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(241, 391);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(147, 34);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Carne Vermelha";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(104, 391);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(129, 34);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Pipoca";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 507);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textAlimento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPotencia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.textTimer);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Microondas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textTimer;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPotencia;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textAlimento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ MicroOndas/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MicroOndas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textAlimento.Text = " ";
+            label5.Text = " ";
+
+            if (labelTimer.Text == "Aquecida")
+            {
+
+                labelTimer.Text = "0";
+                timer1.Enabled = true;
+
+            }
+
+            if ((Convert.ToInt32(labelTimer.Text) + 30) > 120)
+            {
+
+                labelTimer.Text = "120";
+                MessageBox.Show("Valor máximo para aquecimento é de 2 minutos");
+                
+            }
+            else
+            {
+
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) + 30);
+                timer1.Enabled = true;
+                textPotencia.Text = "8";
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            // A propriedade Interval do timer foi alterado para 1000 milisegundos para representar o 1 segundo de decremento
+
+            if (Convert.ToInt32(labelTimer.Text) != 0)
+            {
+
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) - 1);
+
+            }
+            else
+            {
+                timer1.Enabled = false;
+                labelTimer.Text = "Aquecida";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelTimer.Text = "0";
+            textAlimento.CharacterCasing = CharacterCasing.Upper;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textAlimento.Text = " ";
+            label5.Text = " ";
+
+            if (textTimer.Text == "0" || textTimer.Text == string.Empty)
+            {
+
+                MessageBox.Show("O valor minimo é 1 para aquecimento.");
+                return;
+
+            }
+
+            if (Convert.ToInt32(textPotencia.Text) < 1 || Convert.ToInt32(textPotencia.Text) > 10 || textTimer.Text == string.Empty)
+            {
+
+                MessageBox.Show("O valor para potência deve estar entre 1 e 10. Será assumido o valor 10 para padrão.");
+                textPotencia.Text = "10";
+
+            }
+
+
+            if (labelTimer.Text == "Aquecida")
+            {
+
+                labelTimer.Text = "0";
+                timer1.Enabled = true;
+
+            }
+
+            if ((Convert.ToInt32(labelTimer.Text) + Convert.ToInt32(textTimer.Text)) > 120)
+            {
+
+                labelTimer.Text = "120";
+                MessageBox.Show("Valor máximo para aquecimento é de 2 minutos");
+
+            }
+            else
+            {
+
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) + Convert.ToInt32(textTimer.Text));
+                timer1.Enabled = true;
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            labelTimer.Text = "0";
+            //textPotencia.Text = "0";
+            textTimer.Text = "0";
+            textAlimento.Text = " ";
+            timer1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+            } 
+            else
+            {
+                timer1.Enabled = false;
+             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textAlimento.Text == "FRANGO")
+            {
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) + 50);
+                textPotencia.Text = "6";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                label5.Text = "Alimento incompatível com o programa!";
+                textAlimento.Text = " ";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textAlimento.Text == "ARROZ")
+            {
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) + 35);
+                textPotencia.Text = "2";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                label5.Text = "Alimento incompatível com o programa!";
+                textAlimento.Text = " ";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textAlimento.Text == "PIPOCA")
+            {
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) + 25);
+                textPotencia.Text = "3S";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                label5.Text = "Alimento incompatível com o programa!";
+                textAlimento.Text = " ";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textAlimento.Text == "CARNE VERMELHA")
+            {
+                labelTimer.Text = Convert.ToString(Convert.ToInt32(labelTimer.Text) + 90);
+                textPotencia.Text = "9";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                label5.Text = "Alimento incompatível com o programa!";
+                textAlimento.Text = " ";
+            }
+        }
+    }
+}
